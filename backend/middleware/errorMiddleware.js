bd5aceded5a0309a8d6352f1718cc001cf0fd304
Emad Kiani <emadkiani@outlook.com>
2024-05-05 10:35:34 +0330

    fix some small issues

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -10,7 +10,7 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(statusCode).json({
     message: message,
-    stack: process.env.NODE_ENV === 'development' ? err.stack : null,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   })
 }
 
--- backend/utils/calcPrices.js
+++ backend/utils/calcPrices.js
@@ -1,23 +1,23 @@
 export function addDecimals(num) {
-  return (Math.round(num * 100) / 100).toFixed(2);
+  return (Math.round(num * 100) / 100).toFixed(2)
 }
 
 export default function calcPrices(orderItems) {
   const itemsPrice = orderItems.reduce(
     (acc, item) => acc + (item.price * 100 * item.quantity) / 100,
     0
-  );
+  )
 
-  const shippingPrice = itemsPrice > 500 ? 0 : 15;
+  const shippingPrice = itemsPrice > 500 ? 0 : 15
 
-  const taxPrice = 0.09 * itemsPrice;
+  const taxPrice = 0.09 * itemsPrice
 
-  const totalPrice = itemsPrice + shippingPrice + taxPrice;
+  const totalPrice = itemsPrice + shippingPrice + taxPrice
 
   return {
     itemsPrice: addDecimals(itemsPrice),
     taxPrice: addDecimals(taxPrice),
     shippingPrice: addDecimals(shippingPrice),
     totalPrice: addDecimals(totalPrice),
-  };
+  }
 }
